e85bfb927ac39f5d7ddef5c1d12a4557a673aa22
mihauvuong2008 <vungocdongbk@gmail.com>
2017-04-03 21:28:41 +0700

    NEURAL
    demo neural sigmoid

---
+++ NEURAL_AI/Debug/src/NEURAL_AI.d
@@ -0,0 +1,1 @@
+src/NEURAL_AI.o: ../src/NEURAL_AI.cpp
---
+++ NEURAL_APP/src/test.java
@@ -0,0 +1,8 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		// NeuralNetwork<LearningRule> n = new NeuralNetwork<>();
+	}
+
+}
